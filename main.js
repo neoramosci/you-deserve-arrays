a28d055907927ef1397c17b278e01112877b39a4
Irineo Ramos <irineo.ramos@codeimmersives.com>
2021-03-02 09:45:06 -0600

    all functions pass.

--- main.js
+++ main.js
@@ -2,8 +2,37 @@
  * YOUR CODE HERE *
  ******************/
 
+function getFirstItemFrom(arr){
+  return arr[0];
+} 
+
+function getLastItemFrom(arr){
+return arr[arr.length -1];
+}
+
+function getIndex3(arr){
+  if(arr.length < 4){
+    return arr[arr.length -1];
+  } else {
+    return arr[3];
+  }
+}
+
+function  isLongList(arr){
+  return arr.length >= 10;
+}
 
 
+function firstItemIsNumber(arr){
+  const firstItem = arr[0];
+  return typeof firstItem === 'number'
+}
+
+
+function secondCharOfThirdString(arr){
+  const thirdItem = arr[2];
+  return thirdItem[1];
+}
 
 
 
